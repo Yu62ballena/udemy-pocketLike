e625c0f1d0b2d004eeb5d4079aa5c66a5b9f5f13
yu62ballena <yu62ballena@gmail.com>
2025-06-04 14:02:17 +0900

    SidebarUserInfoのimgタグをImageコンポーネントに差し替え

--- app/components/SidebarUserInfo.tsx
+++ app/components/SidebarUserInfo.tsx
@@ -1,4 +1,5 @@
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 function SidebarUserInfo() {
   const { data: session } = useSession();
@@ -9,19 +10,27 @@ function SidebarUserInfo() {
         <div className="mt-auto mb-10 pl-6 pt-8 border-t lg:hidden">
           <div className="flex items-center gap-3">
             {session.user.image ? (
-              <img
-                src={session.user.image}
-                alt="ユーザーアイコン"
-                className="w-12 md:w-14 h-12 md:h-14 rounded-full"
-              />
+              <div className="relative w-12 md:w-14 h-12 md:h-14 flex-shrink-0 rounded-full overflow-hidden">
+                <Image
+                  className="object-cover"
+                  src={session.user.image}
+                  alt="ユーザーアイコン"
+                  fill={true}
+                  sizes="56px"
+                />
+              </div>
             ) : (
-              <div className="w-12 md:w-14 h-12 md:h-14 bg-gray-400 rounded-full flex items-center justify-center text-white font-bold">
+              <div className="w-12 md:w-14 h-12 md:h-14 bg-gray-400 rounded-full flex items-center justify-center text-white font-bold flex-shrink-0">
                 {session.user.name?.[0]?.toUpperCase() || "U"}
               </div>
             )}
             <div>
-              <p className="font-medium text-lg md:text-xl">{session.user.name}</p>
-              <p className="text-xs md:text-lg text-gray-500">{session.user.email}</p>
+              <p className="font-medium text-lg md:text-xl">
+                {session.user.name}
+              </p>
+              <p className="text-xs md:text-lg text-gray-500">
+                {session.user.email}
+              </p>
             </div>
           </div>
 
