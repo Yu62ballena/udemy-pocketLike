646dd94e8585eb2bcd599c78ce16a4bc6fb8fdf8
yu62ballena <yu62ballena@gmail.com>
2025-06-09 23:43:40 +0900

    サイドバーのcssを修正

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -13,10 +13,10 @@ function Sidebar({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) {
     <section
       className={`fixed lg:sticky top-16 lg:top-24 h-[calc(100vh-4rem)] lg:h-[calc(100vh-6rem)] z-50 left-0 bg-white w-4/5 md:w-2/5 lg:w-1/5 ${
         isSidebarOpen ? "translate-x-0" : "-translate-x-full translate3d(0,0,0)"
-      } transform transition-transform will-change-transform duration-200 ease-out lg:translate-x-0 flex flex-col overflow-y-auto`}
+      } transform transition-transform will-change-transform duration-200 ease-out lg:translate-x-0 flex flex-col`}
     >
       {/* メインコンテンツエリア（スクロール対象） */}
-      <div className="flex-1 overflow-y-auto">
+      <div className="flex-1 overflow-y-auto min-h-0">
         <SidebarContents
           title="フィルター"
           contents={filterItems}
@@ -25,7 +25,7 @@ function Sidebar({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) {
       </div>
 
       {/* ユーザー情報エリア（固定） */}
-      <div className="flex-shrink-0">
+      <div className="flex-shrink-0 min-h-fit">
         <SidebarUserInfo />
       </div>
     </section>
