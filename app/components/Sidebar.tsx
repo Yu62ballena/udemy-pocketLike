8726762b8d7e904e2ce71ebc4fdadc71275ab7e5
yu62ballena <yu62ballena@gmail.com>
2025-06-10 00:29:37 +0900

    サイドバーのCSSを修正

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -13,10 +13,10 @@ function Sidebar({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) {
     <section
       className={`fixed lg:sticky top-16 lg:top-24 h-[calc(100vh-4rem)] lg:h-[calc(100vh-6rem)] z-50 left-0 bg-white w-4/5 md:w-2/5 lg:w-1/5 ${
         isSidebarOpen ? "translate-x-0" : "-translate-x-full translate3d(0,0,0)"
-      } transform transition-transform will-change-transform duration-200 ease-out lg:translate-x-0 flex flex-col justify-between`}
+      } transform transition-transform will-change-transform duration-200 ease-out lg:translate-x-0 flex flex-col`}
     >
       {/* メインコンテンツエリア（スクロール対象） */}
-      <div className="overflow-y-auto">
+      <div className="overflow-y-auto mb-10">
         <SidebarContents
           title="フィルター"
           contents={filterItems}
